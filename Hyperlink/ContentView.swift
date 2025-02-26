2d02cef77ddcf6f44e98332f5f458db721f44859
padre <padrewin@colddev.dev>
2025-02-26 11:56:58 +0200

    Initial Commit

---
+++ Hyperlink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hyperlink
+//
+//  Created by padrewin on 26.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hyperlink/HyperlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperlinkApp.swift
+//  Hyperlink
+//
+//  Created by padrewin on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct HyperlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
